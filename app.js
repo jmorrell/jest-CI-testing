9244fff8775d4c3fcd4a9515760f34b3e303b4c6
Jeremy Morrell <jmorrell@heroku.com>
2017-04-13 13:45:12 -0700

    bump

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end(`Hello, world!\n`)
+  res.end(`Hello, cruel world!\n`)
 })
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`))
